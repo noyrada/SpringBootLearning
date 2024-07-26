cbd0bfbc14ba035027dcd41865b98b47ed076e95
noyrada <noyradastudentiq@gmail.com>
2024-07-26 17:12:16 +0700

    RestController added

--- src/main/java/com/project/springboot/SpringBootLearningApplication.java
+++ src/main/java/com/project/springboot/SpringBootLearningApplication.java
@@ -2,11 +2,19 @@ package com.project.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootLearningApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLearningApplication.class, args);
 	}
+	@GetMapping()
+	public String getGreeting(){
+		return "Hello World";
+	}
 }
+
